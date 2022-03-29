a32f2890bdad32cb78f02dae442efa63f7acf0dd
GreedNeSS <ruslan199126@gmail.com>
2022-03-29 19:14:38 +0300

    Using entity fields.

---
+++ EntityField/ApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityField.Models;
+
+namespace EntityField
+{
+    internal class ApplicationContext: DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Employee");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().Property("Id").HasField("id");
+            modelBuilder.Entity<User>().Property("Age").HasField("age");
+            modelBuilder.Entity<User>().Property("name");
+        }
+    }
+}
---
+++ EntityField/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityField.Models
+{
+    internal class User
+    {
+        private string name;
+        private int id;
+        private int age;
+
+        public int Id => id;
+        public int Age => age;
+
+        public User(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {name}, Age: {Age}";
+        }
+    }
+}
